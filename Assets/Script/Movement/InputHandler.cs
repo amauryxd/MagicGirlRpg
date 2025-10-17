d9de481144f71403610a7d21258361fd690b676c
amauryxd <cdmx2472@amerike.edu.mx>
2025-10-17 16:45:34 -0600

    fhkbsd

--- Assets/Script/Movement/InputHandler.cs
+++ Assets/Script/Movement/InputHandler.cs
@@ -4,8 +4,23 @@ using UnityEngine.InputSystem;
 public class InputHandler : MonoBehaviour
 {
     protected Vector2 controllerInput;
+    protected float fightMove;
+    protected bool onConfirm;
+    protected bool onNegate;
     public void OnMove(InputAction.CallbackContext context)
     {
         controllerInput = context.action.ReadValue<Vector2>();
+    }
+    public void OnMoveFight(InputAction.CallbackContext context)
+    {
+       fightMove = context.action.ReadValue<float>();
+    }
+    public void OnConfirm(InputAction.CallbackContext context)
+    {
+        onConfirm = context.performed;
+    }
+    public void OnNegate(InputAction.CallbackContext context)
+    {
+        onNegate = context.performed;
     }
 }
