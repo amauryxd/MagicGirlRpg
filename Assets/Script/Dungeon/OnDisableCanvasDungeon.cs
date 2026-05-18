8f90105f14db02f3fc4ddd858f8177bcc3a85c03
amauryxd <cdmx2472@amerike.edu.mx>
2026-05-17 23:22:31 -0600

    Add dungeon props sprites; update scenes/shaders
    Add a new PropsCosas folder with multiple 2D prop sprites and their import .meta settings (Avion Abajo/Arriba, Cosa1-5, various signs, etc.) and sprite sheets. Add shader animation/controller assets (chaderShido.anim, chaider.controller). Update shader materials (Shadow.mat, WaterShaderMaterial.mat) and modify scenes (Dungeon.unity, Menu.unity) along with a small change to OnDisableCanvasDungeon.cs to integrate the new assets and visual changes.

--- Assets/Script/Dungeon/OnDisableCanvasDungeon.cs
+++ Assets/Script/Dungeon/OnDisableCanvasDungeon.cs
@@ -4,9 +4,11 @@ public class OnDisableCanvasDungeon : MonoBehaviour
 {
     public GameObject canvasToDisable;
     public GameObject toAtivate;
+    public GameObject anotherToDesactivate;
     void OnDisable()
     {
         canvasToDisable.SetActive(false);
         toAtivate.SetActive(true);
+        anotherToDesactivate.SetActive(false);
     }
 }
